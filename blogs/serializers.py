9139066e624bf5cda4d1f184fd9d3e6468964f30
Mohammed FLITI <mohammedfliti02@gmail.com>
2024-01-29 00:05:13 +0100

    blog test

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from .models import Blog
+
 class BlogSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Blog
         fields = '__all__'
+        extra_kwargs = {'image': {'required': False}}
--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Lawyer, Blog
 
-# Create your tests here.
+class BlogCreateViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_blog_post(self):
+        lawyer = Lawyer.objects.create(name='John Doe')
+
+        self.client.force_authenticate(user=lawyer)
+
+      
+        blog_data = {
+            'lawyer': lawyer.id,
+            'title': 'Test Blog Post',
+            'content': 'This is a test blog post content.',
+        }
+
+     
+        response = self.client.post(reverse('create-blog'), data=blog_data, format='json')
+
+       
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+       
+        self.assertEqual(Blog.objects.count(), 1)
+
+      
+        self.assertEqual(Blog.objects.get().title, 'Test Blog Post')
